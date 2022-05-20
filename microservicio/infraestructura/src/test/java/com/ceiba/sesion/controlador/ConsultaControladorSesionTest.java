65ed7b943e778be50028756741276a5f5111bf45
Dairo Antonio Gil Bareño - Ceiba Software <dairo.gil@ceiba.com.co>
2022-05-20 08:59:11 -0500

    Se ajusta archivo sugerido por sonarlint

--- microservicio/infraestructura/src/test/java/com/ceiba/sesion/controlador/ConsultaControladorSesionTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/sesion/controlador/ConsultaControladorSesionTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ConsultaControladorSesion.class)
 @ContextConfiguration(classes = ApplicationMock.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class ConsultaControladorSesionTest {
+class ConsultaControladorSesionTest {
 
     @Autowired
     private MockMvc mockMvc;
